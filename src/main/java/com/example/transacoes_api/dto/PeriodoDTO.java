e2b8444caa19ca8ce1ca0e84281631655de0424a
pauloheiderich <pauloheiderich@mx2.unisc.br>
2025-04-23 21:26:23 -0300

    Update PeriodoDTO.java

--- src/main/java/com/example/transacoes_api/dto/PeriodoDTO.java
+++ src/main/java/com/example/transacoes_api/dto/PeriodoDTO.java
@@ -1,1 +1,27 @@
+package com.example.transacoesapi.dto;
 
+import jakarta.validation.constraints.NotNull;
+
+public class PeriodoDTO {
+    @NotNull
+    private String dataInicial;
+    
+    @NotNull
+    private String dataFinal;
+
+    public String getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(String dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public String getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(String dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+}
