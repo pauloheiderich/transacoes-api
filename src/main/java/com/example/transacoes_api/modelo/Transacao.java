a3d61cb2bfaf460cbe3700b4cadb58d974040539
pauloheiderich <pauloheiderich@mx2.unisc.br>
2025-04-23 21:27:44 -0300

    Update Transacao.java

--- src/main/java/com/example/transacoes_api/modelo/Transacao.java
+++ src/main/java/com/example/transacoes_api/modelo/Transacao.java
@@ -1,1 +1,32 @@
+package com.example.transacoesapi.modelo;
 
+import java.time.LocalDateTime;
+
+public class Transacao {
+    private Double valor;
+    private LocalDateTime dataHora;
+
+    public Transacao() {
+    }
+
+    public Transacao(Double valor, LocalDateTime dataHora) {
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+}
