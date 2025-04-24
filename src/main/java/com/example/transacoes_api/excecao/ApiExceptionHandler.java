8a8d95cb7969ed95fd5642a38d0fe22bbe212c26
pauloheiderich <pauloheiderich@mx2.unisc.br>
2025-04-23 21:23:15 -0300

    Update ApiExceptionHandler.java

--- src/main/java/com/example/transacoes_api/excecao/ApiExceptionHandler.java
+++ src/main/java/com/example/transacoes_api/excecao/ApiExceptionHandler.java
@@ -1,1 +1,22 @@
+package com.example.transacoesapi.excecao;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Void> handleHttpMessageNotReadable() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Void> handleMethodArgumentNotValid() {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+    }
+}
