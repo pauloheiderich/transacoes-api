b738fc2bc46583f842313976d3f098c22214e7fe
pauloheiderich <pauloheiderich@mx2.unisc.br>
2025-04-23 21:26:53 -0300

    Update TransacaoDTO.java

--- src/main/java/com/example/transacoes_api/dto/TransacaoDTO.java
+++ src/main/java/com/example/transacoes_api/dto/TransacaoDTO.java
@@ -1,1 +1,30 @@
+package com.example.transacoesapi.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import java.time.LocalDateTime;
+
+public class TransacaoDTO {
+    @NotNull
+    @PositiveOrZero
+    private Double valor;
+    
+    @NotNull
+    private String dataHora;
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(String dataHora) {
+        this.dataHora = dataHora;
+    }
+}
